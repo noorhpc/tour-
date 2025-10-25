052e888e4f5724670d0347cfdefabdf018363665
noorhpc <noorhpc5033@gmail.com>
2025-10-25 10:32:17 -0700

    123

---
+++ aaa.php
@@ -0,0 +1,6 @@
+Home	Website ka welcome dashboard
+Destinations	Admin panel me nayi places add, user ke liye explore
+Packages	Travel offers + price etc
+Booking	Form jahan se customer book karega
+Contact	Customer queries aayein gi
+Login / Register	Users + Admin login system
